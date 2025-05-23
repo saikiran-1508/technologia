5c3ddb7761e7f710d41a4cb8812e54928aa41c1a
saikiran-1508 <saikiranreddy2608@gmail.com>
2025-05-23 21:25:52 +0530

    Update image.js

--- src/helpers/image.js
+++ src/helpers/image.js
@@ -1,38 +1,51 @@
+// helpers/image.js
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
+import { Image } from 'react-native';
 import Animated from 'react-native-reanimated';
 
 export const CachedImage = (props) => {
   const [cachedSource, setCachedSource] = useState(null);
-  const { uri } = props;
+  const { uri, style } = props;
 
   useEffect(() => {
+    let isMounted = true;
+
     const getCachedImage = async () => {
       try {
         const cachedImageData = await AsyncStorage.getItem(uri);
         if (cachedImageData) {
-          setCachedSource({ uri: cachedImageData });
+          isMounted && setCachedSource({ uri: cachedImageData });
         } else {
           const response = await fetch(uri);
           const imageBlob = await response.blob();
           const base64Data = await new Promise((resolve) => {
             const reader = new FileReader();
             reader.readAsDataURL(imageBlob);
-            reader.onload = () => {
-              resolve(reader.result);
-            };
+            reader.onload = () => resolve(reader.result);
           });
           await AsyncStorage.setItem(uri, base64Data);
-          setCachedSource({ uri: base64Data });
+          isMounted && setCachedSource({ uri: base64Data });
         }
       } catch (error) {
         console.error('Error caching image:', error);
-        setCachedSource({ uri }); // Fallback to original URI if caching fails
+        isMounted && setCachedSource({ uri }); // fallback to original URI
       }
     };
 
     getCachedImage();
-  }, [uri]); // Dependency array includes uri to re-run effect if uri changes
 
-  return <Animated.Image source={cachedSource} {...props} />;
+    return () => {
+      isMounted = false;
+    };
+  }, [uri]);
+
+  return cachedSource ? (
+    <Animated.Image source={cachedSource} style={style} {...props} />
+  ) : (
+    <Image
+      source={require('../assets/placeholder.png')} // Add a local placeholder
+      style={style}
+    />
+  );
 };
